c171c86e853b8ee32a4990b48d8bb84d8ee6c272
xClaudiaArias <xClaudiaArias@gmail.com>
2025-05-12 16:34:13 -0400

    changed favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,15 +9,13 @@ import Footer from "../components/Footer";
 export const metadata = {
   title: "Altus",
   description: "Residential Tower in Los Cerros de Gurabo, Santiago, DR",
-  // TODO: add favicon
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* NOTE: CHANGE THIS  */}
-        <link rel="icon" href="https://i.redd.it/lhg9d9b80lz61.png" />
+        <link rel="icon" href="/assets/logos/logoicon.png" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </head>
       <body className={inter.className}>
