2516c3f2251e289653a1d79b3b60af5e3a5321c9
xClaudiaArias <xClaudiaArias@gmail.com>
2025-05-28 12:22:51 -0400

    renders complete

--- src/app/galeria/renders/page.js
+++ src/app/galeria/renders/page.js
@@ -56,22 +56,30 @@ const images = [
 ]
 
     export default function Slideshow() {
-    const [emblaRef, embla] = useEmblaCarousel({ loop: true });
+    const [emblaRef, embla] = useEmblaCarousel({ loop: true, draggable: true  });
     const [selectedIndex, setSelectedIndex] = useState(0);
 
     const scrollPrev = useCallback(() => embla?.scrollPrev(), [embla]);
     const scrollNext = useCallback(() => embla?.scrollNext(), [embla]);
 
-    const onSelect = useCallback(() => {
+   useEffect(() => {
         if (!embla) return;
-        setSelectedIndex(embla.selectedScrollSnap());
-    }, [embla]);
 
-    useEffect(() => {
-        if (!embla) return;
-        embla.on('select', onSelect);
-        onSelect();
-    }, [embla, onSelect]);
+        const updateIndex = () => {
+            setSelectedIndex(embla.selectedScrollSnap());
+        };
+
+        embla.on('select', updateIndex);
+        embla.on('reInit', updateIndex);
+        updateIndex();
+
+        console.log(selectedIndex, ' selected index')
+
+        return () => {
+            embla.off('select', updateIndex);
+            embla.off('reInit', updateIndex);
+        };
+    }, [embla]);
 
     useEffect(() => {
         const handleKeyDown = (event) => {
@@ -106,13 +114,16 @@ const images = [
             sx={{
             height: '100%',
             width: '100%',
-            overflow: 'hidden',
+            overflowX: { xs: 'auto', md: 'hidden' },
+            overflowY: 'hidden'
             }}
         >
             <Box
             sx={{
                 display: 'flex',
                 height: '100%',
+                minWidth: "100%",
+                scrollSnapType: 'x mandatory'
             }}
             >
             {images.map((image, idx) => (
@@ -120,7 +131,7 @@ const images = [
                 key={idx}
                 sx={{
                     flex: '0 0 100%',
-                    height: '100%',
+                    height: {xs: '100vh', md: '100%'},
                     position: 'relative',
                     backgroundColor: "#fff"
                 }}
@@ -132,7 +143,7 @@ const images = [
                     sx={{
                     height: '100%',
                     width: '100%',
-                    objectFit: 'fill'
+                    objectFit: {xs: 'cover', md: 'fill'}
                     }}
                 />
                 </Box>
@@ -148,9 +159,9 @@ const images = [
             left: 0,
             right: 0,
             transform: 'translateY(-50%)',
-            display: 'flex',
+            display: {xs: 'none', md: 'flex'},
             justifyContent: 'space-between',
-            px: 6,
+            px: {xs: 0, md: 6},
             zIndex: 2,
             }}
         >
@@ -184,18 +195,18 @@ const images = [
         <Box
             sx={{
             position: 'absolute',
-            bottom: 24,
+            bottom: {xs: 80, md: 40},
             left: 24,
             color: 'white',
-            padding: 2,
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            padding: {xs: 1, md: 2},
+            backgroundColor: {xs: 'rgba(0, 0, 0, 0.29)', md: 'rgba(0, 0, 0, 0.5)'},
             borderRadius: 2,
             maxWidth: '80%',
 
             }}
         >
             <Link href="/galeria">
-            <Typography variant="body2" sx={{color: "#fff", textTransform: 'uppercase', fontWeight: '100', display: 'flex', alignItems: 'center', gap: 2, textDecoration: 'underline'}}>
+            <Typography variant="body2" sx={{color: "#fff", textTransform: 'uppercase', fontWeight: '100', display: 'flex', alignItems: 'center', gap: 2, textDecoration: 'underline', fontSize: {xs: '10px', md: '16px'}}}>
                 <ArrowBackIcon />
                 Volver al menú anterior
                 </Typography>
@@ -206,19 +217,19 @@ const images = [
         <Box
             sx={{
             position: 'absolute',
-            bottom: 24,
+            bottom: {xs: 80, md: 40},
             right: 24,
             color: 'white',
             padding: '6px 12px',
             borderRadius: '8px',
             backgroundColor: 'rgba(0, 0, 0, 0.36)',
-            px: 4,
+            px: {xs: 2, md: 4},
             textAlign: 'right'
             }}
         >
             <Typography
             sx={{
-                fontSize: '16px',
+                fontSize: {xs: '10px', md:'16px'},
                 fontWeight: '100',
                 textTransform: 'uppercase'
             }}
@@ -227,7 +238,7 @@ const images = [
             </Typography>
             <Typography
             sx={{
-                fontSize: '30px',
+                fontSize: {xs: '14px', md:'30px'},
                 fontWeight: '100'
             }}
             >
